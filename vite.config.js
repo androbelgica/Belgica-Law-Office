f8a4a9657a95e6e1d7a66dbffa27c6b44ec34d0f
androbelgica <androbelgica85@gmail.com>
2025-07-18 10:07:14 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,29 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            input: ["resources/css/app.css", "resources/js/app.jsx"],
             refresh: true,
         }),
         react(),
     ],
+    server: {
+        https: true, // for local dev only
+    },
+    build: {
+        outDir: "public/build",
+        manifest: true,
+        rollupOptions: {
+            output: {
+                entryFileNames: "[name].js",
+                chunkFileNames: "[name]-[hash].js",
+                assetFileNames: "[name][extname]",
+                // Add this line to force manifest to root of build dir
+                manualChunks: undefined,
+            },
+        },
+    },
 });
